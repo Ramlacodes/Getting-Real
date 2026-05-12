8d88002441302a022aa4f84a4a5eb19e0b1499d0
Natasja <nbko79820@edu.ucl.dk>
2026-05-12 20:13:58 +0200

    IPrisBeregner og PrisBeregner tilføjet (Interface og klasse)

---
+++ Getting Real/PrisBeregner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Getting_Real
+{
+    public class PrisBeregner : IPrisBeregner
+    {
+        
+    public double BeregnPris(Booking booking)
+        {
+            if (booking == null)
+                throw new ArgumentNullException(nameof(booking));
+
+            if (booking.MenuPakke == null)
+                throw new Exception("Booking mangler menu pakke");
+
+            return booking.AntalKuverter * booking.MenuPakke.PrisPrKuvert;
+        }
+
+    }
+}
