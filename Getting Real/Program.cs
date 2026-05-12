a75eaf4132ebed4a3b8bd63543e9e7dbd75a1418
Ramla <ramloabdullah@gmail.com>
2026-05-12 11:28:09 +0200

    Add project files.

---
+++ Getting Real/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Getting_Real
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t***********************************");
+            Console.WriteLine("\tMarrakech Street-Foods Bookingsytem");
+            Console.WriteLine("\t***********************************");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
