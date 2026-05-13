931061014cb4137a1c6b831eeac3256a59e5f3c2
benjampin <benjampin@gmail.com>
2026-05-13 08:52:37 +0200

    Add files via upload

---
+++ Getting Real/MenuPakke.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+// namespace "StreetFood" kan sagtens ændres til noget andet.
+namespace StreetFood
+{
+    public class MenuPakke
+    {
+        // Properties fra DCD.
+        public int PakkeID { get; set; }
+        public string Navn { get; set; }
+        public double PrisPrKuvert { get; set; }
+
+        // Laver liste så pakker kan indeholde flere retter.
+        public List<Ret> Retter { get; set; }
+
+        
+        public MenuPakke(int pakkeID, string navn, double prisPrKuvert)
+        {
+            PakkeID = pakkeID;
+            Navn = navn;
+            PrisPrKuvert = prisPrKuvert;
+            Retter = new List<Ret>();
+        }
+
+        // Tilføjer en ret til pakke.
+        public void TilfoejRet(Ret ret)
+        {
+            Retter.Add(ret);
+        }
+    }
+}
---
+++ Getting Real/Ret.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Igen, namespace "StreetFood" kan ændres.
+namespace StreetFood
+{
+    
+    public class Ret
+    {
+        // Properties fra DCD.
+        public int RetID { get; set; }
+        public string Navn { get; set; }
+        public bool Vegetarisk { get; set; }
+
+       
+        public Ret(int retID, string navn, bool vegetarisk = false)
+        {
+            RetID = retID;
+            Navn = navn;
+            Vegetarisk = vegetarisk;
+        }
+
+        // Viser info om retten.
+        public override string ToString()
+        {
+            // Hvis retten er vegetarisk, tilføjes "Vegetarisk" til navnet. Hvis ikke, vises kun navnet.
+            string veg = Vegetarisk ? " [Vegetarisk]" : "";
+            return $"{Navn}{veg}";
+        }
+    }
+}
