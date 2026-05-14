b6feb95c2b2c9c5c49089124a1742e65f08139f6
Natasja <nbko79820@edu.ucl.dk>
2026-05-14 13:51:54 +0200

    Property EventType ændret

--- Getting Real/Booking.cs
+++ Getting Real/Booking.cs
@@ -7,11 +7,11 @@ namespace Getting_Real
     public class Booking
     {
         public int BookingId { get; set; }
-        public string eventType { get; set; }
+        public string EventType { get; set; }
         public DateTime BookingDato { get; set; }
         public int AntalKuverter { get; set; }
         public string Location { get; set; }
-        public string bookingStatus { get; set; }
+        public string BookingStatus { get; set; }
         public string Comment { get; set; }
         public Customer customer { get; set; }
         
