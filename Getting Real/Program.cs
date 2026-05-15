9964d0e3efd0fe6905415b61c14ab48501586df2
Natasja <nbko79820@edu.ucl.dk>
2026-05-15 12:43:33 +0200

    Validering til telfeonnummer lavet.

--- Getting Real/Program.cs
+++ Getting Real/Program.cs
@@ -9,6 +9,18 @@ namespace Getting_Real
         {
             bool isRunning = true;
 
+            // Opret instanser af repository og calculator klasser
+            BookingRepository bookingRepo = new BookingRepository();
+            MenuPackageRepository menuRepo = new MenuPackageRepository();
+            PriceCalculator calculator = new PriceCalculator();
+
+            // Tilføj menupakker
+            menuRepo.AddMenuPackage(new MenuPackage(1, "Casablanca Kebab Wrap", 135));
+            menuRepo.AddMenuPackage(new MenuPackage(2, "Fez Falafel Wrap", 135));
+            menuRepo.AddMenuPackage(new MenuPackage(3, "Marrakech Chicken Wrap", 135));
+
+
+
             while (isRunning)
             {
                 Console.Clear();
@@ -18,7 +30,7 @@ namespace Getting_Real
                 Console.WriteLine("********************************");
 
                 Console.WriteLine("1. Opret kunde");
-                Console.WriteLine("2. Opret booking kommentar");
+                Console.WriteLine("2. Opret booking");
                 Console.WriteLine("3. Vis menupakker");
                 Console.WriteLine("4. Afslut");
 
@@ -37,7 +49,14 @@ namespace Getting_Real
                         string email = Console.ReadLine();
 
                         Console.WriteLine("Indtast telefonnummer:");
-                        int tlf = Convert.ToInt32(Console.ReadLine());
+
+                        int tlf;
+                        // Validering for at sikre, at input er et gyldigt heltal
+                        while (!int.TryParse(Console.ReadLine(), out tlf))
+                        {
+                            Console.WriteLine("Ugyldigt input! Indtast et tal:");
+                        }
+
 
                         Customer customer1 = new Customer(navn, email, tlf);
 
